98a989d7174bf0b65cf48bcb82e795dd00ee24bb
Timmy <timmy@example.com>
2025-12-04 16:21:08 +0200

    tag added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
-
+// Google Analytics Tag ID
 const siteUrl = "https://kupennia.com";
 
 export const metadata: Metadata = {
